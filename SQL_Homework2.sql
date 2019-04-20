5a52099bb95f721e8abe4a19834a75ce2b1d7c88
tbuck09 <tylerbuck09@gmail.com>
2019-04-19 21:09:24 -0600

    Completed second section of SQL Homework

---
+++ SQL_Homework2.sql
@@ -0,0 +1,7 @@
+SELECT * FROM actor WHERE first_name LIKE "JOE";
+
+SELECT * FROM actor WHERE last_name LIKE "%GEN%";
+
+SELECT * FROM actor WHERE last_name LIKE "%LI%" ORDER BY last_name, first_name;
+
+SELECT country_id, country FROM country WHERE country in ("Afghanistan","Bangladesh","China");
