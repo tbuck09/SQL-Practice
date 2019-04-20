3c40131415237c5dea2a7523be630fe02ef9250e
tbuck09 <tylerbuck09@gmail.com>
2019-04-19 22:19:26 -0600

    Finished section 4 of SQL homework

---
+++ SQL_Homework4.sql
@@ -0,0 +1,14 @@
+SELECT last_name,COUNT(last_name) FROM actor GROUP BY last_name;
+
+SELECT last_name,COUNT(last_name) FROM actor GROUP BY last_name HAVING COUNT(last_name) >= 2;
+
+UPDATE actor
+SET first_name = "HARPO"
+WHERE first_name = "GROUCHO" AND last_name = "WILLIAMS";
+
+
+UPDATE actor
+SET first_name = CASE
+	WHEN first_name = "HARPO" AND last_name = "WILLIAMS" THEN first_name = "GROUCHO"
+    ELSE first_name
+END
