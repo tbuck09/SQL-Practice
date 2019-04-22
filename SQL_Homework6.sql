cc341741393306fe2b907ade285b20f8cd49a6a6
tbuck09 <tylerbuck09@gmail.com>
2019-04-21 18:36:43 -0600

    additional work on Section 6

--- SQL_Homework6.sql
+++ SQL_Homework6.sql
@@ -25,4 +25,21 @@ FROM staff
 JOIN payment
 ON staff.staff_id = payment.staff_id
 WHERE payment_date LIKE "2005-08%"
-GROUP BY payment.staff_id
+GROUP BY payment.staff_id;
+
+SELECT
+	film.title,
+	film_actor.film_id,
+	COUNT(actor_id)
+FROM film_actor
+JOIN film
+ON film_actor.film_id = film.film_id
+GROUP BY film_actor.film_id;
+
+SELECT
+	film.title,
+    COUNT(film.title)
+FROM film
+JOIN inventory
+ON film.film_id = inventory.film_id
+WHERE film.title = "Hunchback Impossible"
